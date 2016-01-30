a6bac08efc29e36a77953cd052a98f706d8b0f64
Yu Chen Hou <houyuchen66@gmail.com>
2016-01-30 16:06:27 -0500

    helpers

--- app/helpers.js
+++ app/helpers.js
@@ -3,5 +3,8 @@ module.exports = {
     const time = new Date().getTime();
     const salt = Math.random() * 1000;
     return new Buffer(Math.floor(time + salt).toString()).toString('base64');
+  },
+  
+  generateRandomEvent: function() {
   }
 };
