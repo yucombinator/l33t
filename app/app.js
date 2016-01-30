6b6fc4dc360465d59e4fc2a969edf7f9a401b62c
Yu Chen Hou <houyuchen66@gmail.com>
2016-01-30 00:34:11 -0500

    enable rendering from server

--- app/app.js
+++ app/app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'ejs');  
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/public');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index', { title: 'The index page!' });
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('App listening on port 3000!');
 });
