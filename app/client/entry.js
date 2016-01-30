9f2813759a2d6fa398283fc383b499b549ccb102
Habeeb Ahmed <1habeeb.ahmed1@gmail.com>
2016-01-30 16:31:47 -0500

    put in score system

--- app/client/entry.js
+++ app/client/entry.js
@@ -4,6 +4,12 @@ import Typer from './Typer.js';
 require('./scss/style.scss');
 require('./scss/crt_style.css');
 
+var MAX_KEY_SPEED = 40;
+var MAX_KEY_UNIQUENESS = 40;
+
+var UNIQUENESS_SCORE_WEIGHT = 0.5;
+var SPEED_SCORE_WEIGHT = 0.5;
+
 var socket = io();
 socket.on('connect', function () {
   socket.emit('joinRoom', {
@@ -15,4 +21,31 @@ socket.on('connect', function () {
   });
 });
 
-var typer = new Typer($("#console"));
+var _this = this;
+
+var mKeyPresses = [];
+var typer = new Typer($("#console"), function(keyCode) {
+	mKeyPresses.push(keyCode);
+});
+
+setInterval(function() {
+	var uniqueCount = mKeyPresses.length == 0 ? 0 : 1; // measure of uniqueness
+	var current = mKeyPresses[0];	
+	mKeyPresses.sort();
+	var i = 0;
+	for (; i < mKeyPresses.length ; i++) {
+		if (mKeyPresses[i] != current) {
+			current = mKeyPresses[i];
+			uniqueCount ++;
+		}
+	}
+	var speed = mKeyPresses.length;
+	// send keys/second
+	speed = speed > MAX_KEY_SPEED ? MAX_KEY_SPEED : speed;
+	uniqueCount = uniqueCount > MAX_KEY_UNIQUENESS ? MAX_KEY_UNIQUENESS : uniqueCount; 
+
+	var score = 100 * UNIQUENESS_SCORE_WEIGHT * uniqueCount/MAX_KEY_UNIQUENESS + 
+				100 * SPEED_SCORE_WEIGHT * speed/MAX_KEY_SPEED;
+	console.log(score);
+	mKeyPresses = [];
+}, 1000); // inizialize timer for sending key press factor over socket
